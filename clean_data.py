7c9e83b6f8adea261c888624678932e24c2070e3
lfsotoc <lfsotoc@unal.edu.co>
2024-03-08 18:48:11 -0500

    intento1

--- clean_data.py
+++ clean_data.py
@@ -5,11 +5,33 @@ import pandas as pd
 
 
 def load_data(input_file):
-    """Lea el archivo usando pandas y devuelva un DataFrame"""
+    df = pd.read_csv(input_file)
+    return df
 
+
+#print(load_data("input.txt"))
 
 def create_fingerprint(df):
     """Cree una nueva columna en el DataFrame que contenga el fingerprint de la columna 'text'"""
+
+    df = df.copy()
+    df["key"] = df["text"]
+    df["key"] = df["key"].str.strip()
+    df["key"] = df["key"].str.lower()
+    df["key"] = df["key"].str.replace("-", "")
+    df["key"] = df["key"].str.translate(
+        str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    )
+    df["key"] = df["key"].str.split()
+    stemmer = nltk.PorterStemmer()
+    df["key"] = df["key"].apply(lambda x: [stemmer.stem(word) for word in x])    
+    df["key"] = df["key"].apply(lambda x: sorted(set(x)))
+    df["key"] = df["key"].str.join(" ")
+
+    return df
+
+
+
 
     # 1. Copie la columna 'text' a la columna 'fingerprint'
     # 2. Remueva los espacios en blanco al principio y al final de la cadena
@@ -26,11 +48,14 @@ def generate_cleaned_column(df):
     """Crea la columna 'cleaned' en el DataFrame"""
 
     df = df.copy()
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
+    keys = df.drop_duplicates(subset="key", keep="first")
+    key_dict = dict(zip(keys["key"], keys["text"]))
+    df["cleaned"] = df["key"].map(key_dict)
 
-    # 1. Ordene el dataframe por 'fingerprint' y 'text'
-    # 2. Seleccione la primera fila de cada grupo de 'fingerprint'
-    # 3.  Cree un diccionario con 'fingerprint' como clave y 'text' como valor
-    # 4. Cree la columna 'cleaned' usando el diccionario
+    return df
+
+
 
 
 def save_data(df, output_file):
@@ -38,9 +63,16 @@ def save_data(df, output_file):
     # Solo contiene una columna llamada 'texto' al igual
     # que en el archivo original pero con los datos limpios
 
+    df = df.copy()
+    df = df[["cleaned"]]
+    df = df.rename(columns={"cleaned": "text"})
+    df.to_csv(output_file, index=False)
+
+
 
 def main(input_file, output_file):
     """Ejecuta la limpieza de datos"""
+
 
     df = load_data(input_file)
     df = create_fingerprint(df)
